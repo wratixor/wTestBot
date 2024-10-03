c3bd14dbb44b69914e9d753a9eaaa99958662265
wratixor <wratixor@gmail.com>
2024-10-03 17:11:26 +0300

    redis install

--- create_bot.py
+++ create_bot.py
@@ -2,17 +2,20 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage
 from decouple import config
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from db_handlers.db_class import PostgresHandler
 
-#pg_db = PostgresHandler(config('PG_LINK'))
-scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
-admins = set(int(admin_id) for admin_id in config('ADMINS').split(','))
+from db_handlers.db_class import PostgresHandler
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+#pg_db = PostgresHandler(config('PG_LINK'))
+storage = RedisStorage.from_url(config('REDIS_LINK'))
+scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
+admins = set(int(admin_id) for admin_id in config('ADMINS').split(','))
+
+
 bot = Bot(token=config('BOT_TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-dp = Dispatcher(storage=MemoryStorage())
+dp = Dispatcher(storage=storage)
