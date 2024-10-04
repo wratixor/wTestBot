2d22d259b3c6cedb61090944d211402759a2f2db
wratixor <wratixor@gmail.com>
2024-10-04 18:54:01 +0300

    aiohttp

--- aiogram_run.py
+++ aiogram_run.py
@@ -68,4 +68,5 @@ async def main():
         await exit(0)
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     asyncio.run(main())
--- create_bot.py
+++ create_bot.py
@@ -2,13 +2,14 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.fsm.storage.redis import RedisStorage
+#from aiogram.fsm.storage.redis import RedisStorage
+from aiogram.fsm.storage.memory import MemoryStorage
 from decouple import config
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#storage = RedisStorage.from_url(config('REDIS_LINK_0'))
+storage = MemoryStorage()
 
-storage = RedisStorage.from_url(config('REDIS_LINK_0'))
 scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
 admins = set(int(admin_id) for admin_id in config('ADMINS').split(','))
 
